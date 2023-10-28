5c17bed8bc51233899446d4c93d9431ce8eb6778
riss-aa <103017800+riss-aa@users.noreply.github.com>
2023-10-28 14:39:50 +0700

    `

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 // import models
-const User = require('../models/user')
+const User = require('../models/u   ser')
 
 // import validation 
 const { registerValidation, loginValidation } = require('../config/validation')
