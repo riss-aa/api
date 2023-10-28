b3a37610d202b18b5cad336992aa5f2234e97072
riss-aa <103017800+riss-aa@users.noreply.github.com>
2023-10-28 14:37:06 +0700

    1

--- app.js
+++ app.js
@@ -18,11 +18,13 @@ const fakultasRoutes = require('./routes/fakultas')
 const ProdiRoutes = require('./routes/prodi')
 const DosenRoutes = require('./routes/dosen')
 const MahasiswaRoutes = require('./routes/mahasiswa')
+const authRoutes = require('./routes/auth')
 // 8. put routes in express
 app.use('/fakultas', fakultasRoutes)
 app.use('/prodi', ProdiRoutes)
 app.use('/dosen', DosenRoutes)
 app.use('/mahasiswa', MahasiswaRoutes)
+app.use('/auth', authRoutes)
 // 3. connect to database
 mongoose.connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true, useUnifiedTopology: true
